9d89cf13ea138656556dd943f66bb33c9d4f3e2f
Spencer Markin <markinspencer@gmail.com>
2018-11-20 23:37:44 -0500

    added temperature conversions functionality

--- src/Update.js
+++ src/Update.js
@@ -1,10 +1,126 @@
 import * as R from "ramda";
 
+const ACTIONS = {
+  LEFT_VALUE_INPUT: "LEFT_VALUE_INPUT",
+  RIGHT_VALUE_INPUT: "RIGHT_VALUE_INPUT",
+  LEFT_UNIT_SELECT: "LEFT_SELECT",
+  LEFT_UNIT_SELECT: "RIGHT_SELECT"
+};
+
+export const leftValueInputAction = value => {
+  return {
+    type: ACTIONS.LEFT_VALUE_INPUT,
+    value
+  };
+};
+
+export const rightValueInputAction = value => {
+  return {
+    type: ACTIONS.RIGHT_VALUE_INPUT,
+    value
+  };
+};
+
+export const leftUnitSelectAction = value => {
+  return {
+    type: ACTIONS.LEFT_UNIT_SELECT,
+    value
+  };
+};
+
+export const rightUnitSelectAction = value => {
+  return {
+    type: ACTIONS.RIGHT_UNIT_SELECT,
+    value
+  };
+};
+
 const update = (action, model) => {
-  switch (action) {
+  switch (action.type) {
+    case ACTIONS.LEFT_VALUE_INPUT: {
+      if (action.value === "")
+        return { ...model, sourceLeft: true, leftValue: "", rightValue: "" };
+
+      const leftValue = toInt(action.value);
+      return convert({ ...model, leftValue, sourceLeft: true });
+    }
+    case ACTIONS.RIGHT_VALUE_INPUT: {
+      if (action.value === "")
+        return { ...model, sourceLeft: false, leftValue: "", rightValue: "" };
+
+      const rightValue = toInt(action.value);
+      return convert({ ...model, rightValue, sourceLeft: false });
+    }
+    case ACTIONS.LEFT_UNIT_SELECT: {
+      const { value: leftUnit } = action;
+      return convert({ ...model, leftUnit });
+    }
+    case ACTIONS.RIGHT_UNIT_SELECT: {
+      const { value: rightUnit } = action;
+      return convert({ ...model, rightUnit });
+    }
     default: {
       return model;
     }
+  }
+};
+
+const toInt = R.pipe(
+  parseInt,
+  R.defaultTo(0)
+);
+
+const round = number => Math.round(number * 10) / 10;
+
+const convert = model => {
+  const { leftValue, leftUnit, rightValue, rightUnit } = model;
+
+  const [fromUnit, fromTemp, toUnit] = model.sourceLeft
+    ? [leftUnit, leftValue, rightUnit]
+    : [rightUnit, rightValue, leftUnit];
+
+  const convertedValue = R.pipe(
+    convertFromToTemp,
+    round
+  )(fromUnit, toUnit, fromTemp);
+
+  return model.sourceLeft
+    ? { ...model, rightValue: convertedValue }
+    : { ...model, leftValue: convertedValue };
+};
+
+const convertFromToTemp = (fromUnit, toUnit, temp) => {
+  const convertFn = R.pathOr(R.identity, [fromUnit, toUnit], UnitConversions);
+  return convertFn(temp);
+};
+
+const CtoF = temp => (9 / 5) * temp + 32;
+const FtoC = temp => (5 / 9) * (temp - 32);
+const KtoC = temp => temp - 273.15;
+const CtoK = temp => temp + 273.15;
+
+const FtoK = R.pipe(
+  FtoC,
+  CtoK
+);
+
+const KtoF = R.pipe(
+  KtoC,
+  CtoF
+);
+
+const UnitConversions = {
+  Celsius: {
+    Fahrenheit: CtoF,
+    Kelvin: CtoK
+  },
+  Fahrenheit: {
+    Celsius: FtoC,
+    Kelvin: FtoK
+  },
+  Kelvin: {
+    Celsius: KtoC,
+    Fahrenheit: KtoF
   }
 };
 
--- src/View.js
+++ src/View.js
@@ -1,6 +1,12 @@
 import * as R from "ramda";
 import hh from "hyperscript-helpers";
 import { h } from "virtual-dom";
+import {
+  leftValueInputAction,
+  rightValueInputAction,
+  leftUnitSelectAction,
+  rightUnitSelectAction
+} from "./Update";
 
 const { div, h1, pre, select, option, input } = hh(h);
 
@@ -12,16 +18,18 @@ const unitOptions = selectedUnit =>
     UNITS
   );
 
-const unitSection = (dispatch, unit, value) =>
-  div({ className: "w-50 ma1" }, [
+const unitSection = (dispatch, unit, value, inputAction, selectAction) =>
+  div({ className: "w-40 ma1" }, [
     input({
+      value,
       type: "text",
       className: "db w-100 mv2 pa2 input-reset ba",
-      value
+      oninput: e => dispatch(inputAction(e.target.value))
     }),
     select(
       {
-        className: "db w-100 pa2 ba input-reset br1 bg-white ba b--black"
+        className: "db w-100 pa2 ba input-reset br1 bg-white ba b--black",
+        onchange: e => dispatch(selectAction(e.target.value))
       },
       unitOptions(unit)
     )
@@ -31,10 +39,23 @@ const view = (dispatch, model) => {
   return div({ className: "mw6 center" }, [
     h1({ className: "f2 pv2 bb" }, "Temperature Unit Converter"),
     div({ className: "flex" }, [
-      unitSection(dispatch, model.leftUnit, model.leftValue),
-      unitSection(dispatch, model.rightUnit, model.rightValue)
-    ]),
-    pre(JSON.stringify(model, null, 2)) // only for development
+      unitSection(
+        dispatch,
+        model.leftUnit,
+        model.leftValue,
+        leftValueInputAction,
+        leftUnitSelectAction
+      ),
+      div({ className: "w-20 mt3 tc" }, "="),
+      unitSection(
+        dispatch,
+        model.rightUnit,
+        model.rightValue,
+        rightValueInputAction,
+        rightUnitSelectAction
+      )
+    ])
+    //pre(JSON.stringify(model, null, 2)) // uncomment in development
   ]);
 };
 
